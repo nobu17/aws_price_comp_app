2072226d6b3b982109cac7b782ef20f18159bc8c
nobu17 <lunacy171710@yahoo.co.jp>
2019-09-22 12:54:07 +0900

    add bookoff define

--- common/info/url.go
+++ common/info/url.go
@@ -2,9 +2,15 @@ package info
 
 // amazonURLBase amazon product url.
 const amazonURLBase = "https://www.amazon.co.jp/gp/offer-listing/"
+const amazonStoreType = "amazon"
 
 // surugayaURLBase surugaya product rul.
 const surugayaURLBase = "https://www.suruga-ya.jp/product/detail/"
+const surugayaStoreType = "surugaya"
+
+// bookoffURLBase bookoff online url.
+const bookoffURLBase = "http://www.bookoffonline.co.jp/old/"
+const bookoffStoreType = "bookoff"
 
 // GetAmazonPrdocutURL get prdocut url of amazon for market place.
 func GetAmazonPrdocutURL(productID string) string {
@@ -16,9 +22,14 @@ func GetSurugayaPrdocutURL(productID string) string {
 	return surugayaURLBase + productID
 }
 
+// GetBookoffPrdocutURL get prdocut url of surugaya product place.
+func GetBookoffPrdocutURL(productID string) string {
+	return bookoffURLBase + productID
+}
+
 // IsStoreTypeAmazon judge is store type is amazon.
 func IsStoreTypeAmazon(storeType string) bool {
-	if storeType == "amazon" {
+	if storeType == amazonStoreType {
 		return true
 	}
 	return false
@@ -26,7 +37,15 @@ func IsStoreTypeAmazon(storeType string) bool {
 
 // IsStoreTypeSurugaya judge is store type is surugaya.
 func IsStoreTypeSurugaya(storeType string) bool {
-	if storeType == "surugaya" {
+	if storeType == surugayaStoreType {
+		return true
+	}
+	return false
+}
+
+// IsStoreTypeBookoff judge is store type is bookoff.
+func IsStoreTypeBookoff(storeType string) bool {
+	if storeType == bookoffStoreType {
 		return true
 	}
 	return false
